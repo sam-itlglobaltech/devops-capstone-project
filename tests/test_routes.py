b03cee4c0d5d2b22c2fe6cc55a49b3229cfcba07
sam-itlglobaltech <samuel@itlglobal.tech>
2025-01-18 01:19:59 -0500

    kubernetes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -11,7 +11,7 @@ from unittest import TestCase
 from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
-from service.routes import app
+from service.routes import app, reset_counters
 from service import talisman
 
 DATABASE_URI = os.getenv(
